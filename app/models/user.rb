34ff1d7679b3fa5890ca47a02527f3cbc8aaaaff
chie-python <momochie0712@gmail.com>
2023-04-28 23:57:16 +0900

    userモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/}
+  validates :last_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/}
   validates :birthday, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "は半角英数を両方含む必要があります"}
+
 
 end
