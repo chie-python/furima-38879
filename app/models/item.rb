a6ee93acf42718cb4614b09c3d33d3d71f90c31f
chie-python <momochie0712@gmail.com>
2023-05-05 08:38:59 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_condition
+  belongs_to :shipping_charge
+  belongs_to :ship_area
+  belongs_to :ship_day
   belongs_to :user
   has_one_attached :image
+
+  validates :item_category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :ship_area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :ship_day_id, numericality: { other_than: 1, message:"can't be blank" }
+
 end
