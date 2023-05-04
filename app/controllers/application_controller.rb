d00f84db6c196a35811f5fb98f5b6273ab687e5f
chie-python <momochie0712@gmail.com>
2023-05-04 20:19:00 +0900

    ユーザー管理機能の修正#3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :configre_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameter, if: :devise_controller?
   private
 
-  def configre_permitted_parameters
+  def configure_permitted_parameter
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
   end
 end
