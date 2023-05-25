0ae5215eb170acff0f92ae7332211fe04224e444
chie-python <momochie0712@gmail.com>
2023-05-25 23:23:37 +0900

    deploy.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.17.3'
 
 # Capistranoのログの表示に利用する
-set :application, 'futima-38879'
+set :application, 'furima-38879'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:chie-python/furima-38879.git'
