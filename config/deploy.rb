cda5d24a9a18ec005d803e279a1a92c1999ae5e6
chie-python <momochie0712@gmail.com>
2023-05-25 23:40:39 +0900

    deploy.rbの修正#2

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,9 @@ set :application, 'furima-38879'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:chie-python/furima-38879.git'
 
+# masterブランチからmainブランチへ指定の変更
+set :branch, "main"
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
